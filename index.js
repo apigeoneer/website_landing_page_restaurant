33b1a48a094c79548de41947d7ae6e266534d5a1
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-02-09 22:56:30 +0530

    added the drop down functionality with JS

---
+++ index.js
@@ -0,0 +1,16 @@
+const toggle = document.querySelector("#toggle");
+const menu = document.querySelector("#menu");
+
+/* toggle menu- mobile */
+function toggleMenu() {
+    if(menu.classList.contains("active")) { //menu is open
+        menu.classList.remove("active");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+
+}
+
+toggle.addEventListener("click", toggleMenu, false);
